d907edff4ccb09bd64cee8b9523192c42abf98a9
Elijah R <Eli.Reid826@gmail.com>
2023-07-06 19:47:57 -0400

    main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,32 +3,32 @@ import os
 
 csv_file = os.path.join("Resources", "budget_data.csv")
 
-#read csv file, count the months, and the total $
+#read CSV file, count the months, and calculate the total $$
 budget_df = pd.read_csv(csv_file)
-total_months =budget_df.shape[0]
+total_months = budget_df.shape[0]
 total = budget_df["Profit/Losses"].sum()
 
-#calulations of max, min, average, greatest increase and decrease
+#calculations of max, min, average, greatest increase, and decrease
 budget_df["Change"] = budget_df["Profit/Losses"].diff()
 average = budget_df["Change"].mean()
 greatest_increase = budget_df["Change"].max()
 greatest_decrease = budget_df["Change"].min()
-greatest_increase_date = budget_df.loc[budget_df["Change"]== greatest_increase, "Date"].item()
-greatest_decrease_date = budget_df.loc[budget_df["Change"]== greatest_decrease, "Date"].item()
+greatest_increase_date = budget_df.loc[budget_df["Change"] == greatest_increase, "Date"].item()
+greatest_decrease_date = budget_df.loc[budget_df["Change"] == greatest_decrease, "Date"].item()
 
-#output define
-output = (
-    f"Financial Analysis\n"
-    f"-----------------------\n"
-    f"Total Months:{total_months}\n"
-    f"Total: ${total}\n"
-    f"Average Change: ${average:.2f}\n"
-    f'Greatest Increase in Profits: {greatest_increase_date}(${greatest_increase})\n'
-    f'Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})\n'
-)
+#output definition
+output = [
+    "Financial Analysis",
+    "-----------------------",
+    f"Total Months: {total_months}",
+    f"Total: ${total}",
+    f"Average Change: ${average:.2f}",
+    f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})",
+    f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})",
+]
 
 #print
-print(output)
+print("\n".join(output))
 
 #export
 file_to_output = os.path.join("resources", "analysis", "Financial_Analysis.txt")
